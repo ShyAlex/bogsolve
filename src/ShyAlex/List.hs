9e37775e38c9bbe7576e0333186d0a84829601f2
Alex Humphrey <alexejhumphrey@hotmail.com>
2012-06-16 19:44:31 +0100

    Removed unused removeAt function.

--- src/ShyAlex/List.hs
+++ src/ShyAlex/List.hs
@@ -1,17 +1,9 @@
 module ShyAlex.List
-(mapi
-,removeAt
-,groupN) where
-
-import System.Random(RandomGen, randomR)
+(groupN
+,mapi) where
 
 mapi :: (Int -> a -> b) -> [a] -> [b]
 mapi f = zipWith f [0..]
-
-removeAt :: Int -> [a] -> [a]
-removeAt _ [] = []
-removeAt 0 (x:xs) = xs
-removeAt i (x:xs) = x : removeAt (i - 1) xs
 
 groupN :: Int -> [a] -> [[a]]
 groupN _ [] = []
