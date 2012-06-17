81b6266b0e57030588e5ae6f40810cd864d32242
Alex Humphrey <alexejhumphrey@hotmail.com>
2012-06-17 22:02:08 +0100

    Now using an output format which includes the number of points for each word and how the word can be created on the grid.

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,34 @@
-import Data.Char(toLower)
+import Data.Char(toLower, toUpper)
 import Data.List(group, intersperse, sort)
 import ShyAlex.Boggle
+import ShyAlex.List(groupN, padr)
 import System.Environment(getArgs)
 
-getDiceRoutes :: [Dice] -> [String]
-getDiceRoutes [] = []
-getDiceRoutes (ds:dss) = do
+getGridStrings :: Dice -> Dice -> [String]
+getGridStrings allDice wordDice =
+    let faces = map (\ d -> map (if d `elem` wordDice then toUpper else toLower) $ face d) allDice
+        maxFaceWidth = maximum $ map length faces
+        paddedFaces = map (padr (maxFaceWidth + 1) ' ') faces
+        groupedFaces = groupN 4 paddedFaces
+    in map concat groupedFaces
+
+getRouteString :: Dice -> String
+getRouteString ds =
     let route = concat $ intersperse " -> " $ map dieToStr ds
-        output = getWord ds ++ " : " ++ route
-    output : getDiceRoutes dss
+        wordStr = getWord ds
+        pointsStr = show $ getPoints wordStr
+    in wordStr ++ " (" ++ pointsStr ++ " points) : " ++ route
     where dieToStr (Die x y f) = f ++ " (" ++ show x ++ ", " ++ show y ++ ")"
+
+printGrid :: Dice -> Dice -> IO ()
+printGrid allDice wordDice = do
+    let (g:gs) = getGridStrings allDice wordDice
+        routeString = getRouteString wordDice
+        topLine = routeString ++ "    " ++ g
+        otherLines = map (replicate (length routeString + 4) ' ' ++) gs
+    putStrLn topLine
+    mapM putStrLn otherLines
+    putStrLn ""
 
 main = do
     (dictionaryFile : faces) <- getArgs
@@ -17,5 +36,5 @@ main = do
     let dice = toDice (map (map toLower) faces)
         solution = solve dice dictionary
         pointsString = show $ foldl (+) 0 $ map (getPoints . head) $ group $ map getWord solution
-    mapM putStrLn $ getDiceRoutes solution
+    mapM (printGrid dice) solution
     putStrLn $ "Available points: " ++ pointsString
