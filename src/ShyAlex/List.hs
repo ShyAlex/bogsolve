df3661c15d5a3dcfd1aa4d7fc3e3d6ccf9b48ac4
Alex Humphrey <alexejhumphrey@hotmail.com>
2012-06-19 18:54:01 +0100

    Organised ShyAlex.List functions so they're now in alphabetical order.

--- src/ShyAlex/List.hs
+++ src/ShyAlex/List.hs
@@ -5,18 +5,18 @@ module ShyAlex.List
 ,padr
 ,startsWith) where
 
+filterSorted :: (a -> Bool) -> [a] -> [a]
+filterSorted fil = takeWhile fil . dropWhile (not . fil)
+
+groupN :: Int -> [a] -> [[a]]
+groupN _ [] = []
+groupN i l = let (grp, l') = splitAt i l in grp : (groupN i l')
+
 mapi :: (Int -> a -> b) -> [a] -> [b]
 mapi f = zipWith f [0..]
 
 padr :: Int -> a -> [a] -> [a]
 padr n x xs = xs ++ replicate (n - length xs) x
 
-groupN :: Int -> [a] -> [[a]]
-groupN _ [] = []
-groupN i l = let (grp, l') = splitAt i l in grp : (groupN i l')
-
 startsWith :: Eq a => [a] -> [a] -> Bool
 startsWith xs ys = (length xs <= length ys) && (all id $ zipWith (==) xs ys)
-
-filterSorted :: (a -> Bool) -> [a] -> [a]
-filterSorted fil = takeWhile fil . dropWhile (not . fil)
